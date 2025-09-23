d339834d26e0e20be48c807b003bf2b2102a38ac
ZainabZaman <zainzam5@gmail.com>
2025-09-23 14:57:30 +0500

    updated permissions

--- main.tf
+++ main.tf
@@ -78,3 +78,6 @@ resource "azurerm_linux_web_app" "app" {
 output "app_service_url" {
   value = azurerm_linux_web_app.app.default_hostname
 }
+output "app_service_name" {
+  value = azurerm_linux_web_app.app.name
+}
