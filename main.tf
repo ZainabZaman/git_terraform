c193a4172b715e5dfa97e6cee6eeccbbf9bba351
ZainabZaman <zainzam5@gmail.com>
2025-09-23 14:02:17 +0500

    updated permissions

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_key_vault_access_policy" "policy" {
   tenant_id    = data.azurerm_client_config.current.tenant_id
   object_id    = data.azurerm_client_config.current.object_id
 
-  secret_permissions = ["Get", "List"]
+  secret_permissions = ["Get", "List", "Set"]
 }
 
 resource "azurerm_key_vault_secret" "new_secret" {
