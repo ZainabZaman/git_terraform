b414cdd16d360c8e7b431cbc4b1e7ff5ebc60783
ZainabZaman <zainzam5@gmail.com>
2025-09-24 16:30:00 +0500

    deploying vm through terraform

--- main.tf
+++ main.tf
@@ -14,149 +14,67 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
-  name     = "terraform_test"
+  name     = "fastapi-rg"
   location = "East Asia"
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "terraform-test-vnet"
-  address_space       = ["10.0.0.0/16"]
+resource "azurerm_service_plan" "plan" {
+  name                = "fastapi-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "B1"
+  os_type             = "Linux"
 }
 
-# Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-# Public IP
-resource "azurerm_public_ip" "pip" {
-  name                = "terraform-test-pip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-  sku                = "Basic"
-}
-
-# Network Security Group
-resource "azurerm_network_security_group" "nsg" {
-  name                = "terraform-test-nsg"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "FastAPI"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8000"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# Network Interface
-resource "azurerm_network_interface" "nic" {
-  name                = "terraform-test-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip.id
-  }
-}
-
-# Network Security Group Association
-resource "azurerm_network_interface_security_group_association" "nsg_association" {
-  network_interface_id      = azurerm_network_interface.nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-
-# Key Vault (existing)
 resource "azurerm_key_vault" "kv" {
   name                = "fastapikv123"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
+}
 
-  # Enable soft delete recovery
-  soft_delete_retention_days = 7
-  purge_protection_enabled   = false
+resource "azurerm_key_vault_access_policy" "policy" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Recover",
-      "Purge"
-    ]
-  }
+  secret_permissions = ["Get", "List", "Set"]
 }
 
 resource "azurerm_key_vault_secret" "new_secret" {
   name         = "new-secret"
   value        = "super-secret-value"
   key_vault_id = azurerm_key_vault.kv.id
+
+  depends_on = [azurerm_key_vault_access_policy.policy]
 }
 
-# Virtual Machine
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "terraform-test"
-  resource_group_name = azurerm_resource_group.rg.name
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "fastapi-app-service-${random_string.suffix.result}"
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.plan.id
 
-  # Disable password authentication
-  disable_password_authentication = true
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
 
-  network_interface_ids = [
-    azurerm_network_interface.nic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub") # You'll need to create this
+    app_command_line = "python -m uvicorn main:app --host 0.0.0.0 --port 8002"
   }
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+  app_settings = {
+    "WEBSITES_PORT"                    = "8000"
+    "MY_SECRET"                       = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.new_secret.id})"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "true"
+    "ENABLE_ORYX_BUILD"              = "true"
   }
 
   identity {
@@ -164,19 +82,19 @@ resource "azurerm_linux_virtual_machine" "vm" {
   }
 }
 
-# Grant VM access to Key Vault
-resource "azurerm_key_vault_access_policy" "vm_policy" {
+# Grant the App Service access to Key Vault
+resource "azurerm_key_vault_access_policy" "app_policy" {
   key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = azurerm_linux_virtual_machine.vm.identity[0].tenant_id
-  object_id    = azurerm_linux_virtual_machine.vm.identity[0].principal_id
+  tenant_id    = azurerm_linux_web_app.app.identity[0].tenant_id
+  object_id    = azurerm_linux_web_app.app.identity[0].principal_id
 
   secret_permissions = ["Get", "List"]
 }
 
-output "vm_public_ip" {
-  value = azurerm_public_ip.pip.ip_address
+output "app_service_url" {
+  value = "https://${azurerm_linux_web_app.app.default_hostname}"
 }
 
-output "ssh_command" {
-  value = "ssh azureuser@${azurerm_public_ip.pip.ip_address}"
+output "app_service_name" {
+  value = azurerm_linux_web_app.app.name
 }
