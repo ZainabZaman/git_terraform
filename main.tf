9540b323ce40e0f794f8655aaa476c4bf0178a61
ZainabZaman <zainzam5@gmail.com>
2025-10-07 11:08:59 +0500

    create VM, keyvalut and return credentials

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "azurerm_client_config" "current" {}
 # Create Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "Partfiniti-AI-RG"
-  location = "eastus"
+  location = "eastasia"
 
   lifecycle {
     prevent_destroy = false
